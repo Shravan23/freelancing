6984e704fe4d78fa12aa8221f321ddc72481b10d
Shravan Bachu <bachs01@pfw.edu>
2023-12-07 11:24:25 -0500

    Update SecurityConfiguration.java

--- src/main/java/com/acs560/freelancing/configuration/SecurityConfiguration.java
+++ src/main/java/com/acs560/freelancing/configuration/SecurityConfiguration.java
@@ -17,6 +17,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -28,7 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-@Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors().and()
